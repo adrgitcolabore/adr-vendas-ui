88b744b5fbbb08757abbd77384432065708cd9d8
André luis <luis.alms91@gmail.com>
2018-07-24 10:31:30 -0300

    Configuração CSS, Criação do navbar e vendas-listagem/ intalação primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
